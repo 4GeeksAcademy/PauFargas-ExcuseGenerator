00391647084700002059762bda0b8348b62ad380
Pau Fargas <142971403+PauFargas@users.noreply.github.com>
2023-10-13 18:40:13 +0000

    Actual

--- src/app.js
+++ src/app.js
@@ -5,20 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  let who = ["The dog", "My grandma", "My turtle", "My bird"];
-  let action = ["ate", "peed on", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-
-  //write your code here
-
+window.onload = function generateExcuse() {
   let whoRandom = who[Math.floor(Math.random() * who.length)];
   let actionRandom = action[Math.floor(Math.random() * action.length)];
   let whatRandom = what[Math.floor(Math.random() * what.length)];
@@ -37,3 +24,13 @@ window.onload = function() {
     .getElementById("generarExcusa")
     .addEventListener("click", window.onload);
 };
+let who = ["The dog", "My grandma", "My turtle", "My bird"];
+let action = ["ate", "peed on", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
