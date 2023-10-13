37f830edccebaa7ec81ac7bc0662334afc05f586
Pau Fargas <142971403+PauFargas@users.noreply.github.com>
2023-10-13 18:09:42 +0000

    Con boton

--- src/app.js
+++ src/app.js
@@ -5,26 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["The dog", "My grandma", "My turtle", "My bird"];
-let action = ["ate", "peed on", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
-let when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
-];
-
 window.onload = function() {
+  let who = ["The dog", "My grandma", "My turtle", "My bird"];
+  let action = ["ate", "peed on", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
   //write your code here
 
   let whoRandom = who[Math.floor(Math.random() * who.length)];
   let actionRandom = action[Math.floor(Math.random() * action.length)];
   let whatRandom = what[Math.floor(Math.random() * what.length)];
   let whenRandom = when[Math.floor(Math.random() * when.length)];
-  let excuse =
-    "Sorry, " +
+  let excusaGenerada =
+    "Sorry... " +
     whoRandom +
     " " +
     actionRandom +
@@ -32,7 +32,8 @@ window.onload = function() {
     whatRandom +
     " " +
     whenRandom;
-  let element = document.getElementById("excuse");
-  element.innerHTML = excuse;
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = excusaGenerada;
+  document
+    .getElementById("generarExcusa")
+    .addEventListener("click", window.onload);
 };
